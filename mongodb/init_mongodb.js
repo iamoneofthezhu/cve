95250429f7a40463b969623f2ecd7f02203a6066
Ya Lin Zhu <oneofthezhu@gmail.com>
2026-01-09 15:47:19 -0500

    Added json schema for initial collection creation

--- mongodb/init_mongodb.js
+++ mongodb/init_mongodb.js
@@ -10,7 +10,7 @@ function getSecret(filePath) {
   }
 }
 
-//Read secrets from files instead of using environment variables
+//Read secrets from files instead of using environment variables (Tried using environment variables but variables not set by the time this script runs)
 //No need to create root user in admin database here because it is already created by docker as defined in docker-compose.yml
 var appUser = getSecret('/etc/mongo/db_user_username.txt');
 var appPwd = getSecret('/etc/mongo/db_user_password.txt');
@@ -39,10 +39,76 @@ if (userInfo.users && userInfo.users.length > 0) {
   createUsersInAdminDatabase(appUser, appPwd);
 }
 
+//need to create a collection in the database so the database shows up in the mongo shell
+//add validation to the collection to ensure the data is in the correct format
 const targetDb = db.getSiblingDB(dbName);
 if (!targetDb.getCollectionNames().includes("cve_collection")) {
   print(`Creating initial collection in ${dbName}...`);
-  targetDb.createCollection("cve_collection");
+  targetDb.createCollection("cve_collection", {
+    validator: {
+      $jsonSchema: {
+        bsonType: "object",
+        required: ["cve_id", "published", "status", "description"],
+        properties: {
+          cve_id: { bsonType: "string" },
+          published: { bsonType: "string" },
+          status: { bsonType: "string" },
+          description: { bsonType: "string" },
+          cvss: {
+            bsonType: "object",
+            properties: {
+              "v31": {
+                bsonType: "object",
+                properties: {
+                  "base_score": { bsonType: "number" },
+                  "severity": { bsonType: "string" },
+                  "vector": { bsonType: "string" },
+                  "exploitability": { bsonType: "number" },
+                  "impact": { bsonType: "number" }
+                }
+              },
+              "v40": {
+                bsonType: "object",
+                properties: {
+                  "base_score": { bsonType: "number" },
+                  "severity": { bsonType: "string" },
+                  "vector": { bsonType: "string" },
+                  "exploitability": { bsonType: "number" }
+                }
+              }
+            }
+          },
+          "weaknesses": { 
+            bsonType: "array",
+            items: {
+              bsonType: "string"
+            }
+          },
+          "affected": {
+            bsonType: "array",
+            items: {
+              bsonType: "object",
+              properties: {
+                "vendor": { bsonType: "string" },
+                "product": { bsonType: "string" },
+                "version": { bsonType: "string" }
+              }
+            }
+          },
+          "references": {
+            bsonType: "array",
+            items: {
+              bsonType: "object",
+              properties: {
+                url: { bsonType: "string" }
+              }
+            }
+          }
+        }
+      }
+    }
+  });
+  
   print("Database '" + dbName + "' created.");
 }
 
